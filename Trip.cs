4b086e03a69d2312e18147ed7583b848be1417c2
Swendude <swenmulderij@gmail.com>
2014-12-23 16:23:04 +0100

    init

---
+++ Trip.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    class Trip
+    {
+        public String dep_station;
+        public String arr_station;
+        public int tarief_eenheden;
+        public float price;
+        public Trip(String dep, String arr)
+        {
+            dep_station = dep;
+            arr_station = arr;
+        }
+        public int getDistance()
+        {
+            tarief_eenheden = Tariefeenheden.getTariefeenheden(dep_station, arr_station);
+            return tarief_eenheden;
+        } 
+        public float getPrice()
+        {
+            // We always take the first column and calculate a discount percentage afterwards!
+            return PricingTable.getPrice(tarief_eenheden,0);
+        }
+    }
+}
